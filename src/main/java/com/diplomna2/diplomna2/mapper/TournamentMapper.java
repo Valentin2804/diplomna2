ee4f67293d59baf9c14c48ac56482ed01c6d98fb
Lenovo <valeninmarinov05@gmail.com>
2023-05-31 00:00:28 +0300

    kur

--- src/main/java/com/diplomna2/diplomna2/mapper/TournamentMapper.java
+++ src/main/java/com/diplomna2/diplomna2/mapper/TournamentMapper.java
@@ -3,6 +3,7 @@ package com.diplomna2.diplomna2.mapper;
 import com.diplomna2.diplomna2.entity.Tournaments;
 import com.diplomna2.diplomna2.controller.resources.TournamentsResource;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(uses = {TournamentMapper.class, ManagerMapper.class})
@@ -10,7 +11,9 @@ public interface TournamentMapper {
 
     TournamentMapper TOURNAMENT_MAPPER = Mappers.getMapper(TournamentMapper.class);
 
+    @Mapping(target = "participants.name", source = "tournamentsResource.participants")
     Tournaments fromTournamentsResource(TournamentsResource tournamentResource);
 
+    @Mapping(target = "participants", source = "tournaments.participants.name")
     TournamentsResource toTournamentsResource(Tournaments tournament);
 }
